18de688d6733ec0a0f430a67a78fc4eda938210e
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-12-25 13:51:11 +0530

    fix: bubble_sort (Stable Sort)

--- 03-Day/bubble_sort/Main.java
+++ 03-Day/bubble_sort/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j + 1] < arr[j]) {
+                if (arr[j + 1] <= arr[j]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
